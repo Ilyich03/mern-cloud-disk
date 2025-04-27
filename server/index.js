c5861c5948cff5928493a888f82cd22a24b7257c
V Stepanov <valstepanov9@gmail.com>
2025-04-27 08:56:03 -0400

    create server and add user model

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
- console.log("Hello")
+const express =  require ("express")
+const mongoose = require ("mongoose")
+const config = require ("config")
+
+const app = express()
+const PORT = config.get('serverPort')
+
+
+
+const start = async () => {
+    try{
+        await mongoose.connect(config.get('dbUrl'))
+
+        app.listen(PORT, () => {
+            console.log('Server start on Port ', PORT)
+        })
+    } catch (e) {
+        console.log((e))
+    }
+}
+
+start()
